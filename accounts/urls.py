86679d071999f3badb5f0354791262e9306250e5
Jhonattan <millanjhonattan0@gmail.com>
2020-08-04 13:47:45 -0400

    Add change password page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,5 +9,14 @@ urlpatterns = [
 	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 	path('signup/', views.SignUpView.as_view(), name='signup'),
 	path('settings/', views.UserUpdateView.as_view(), name='settings'),
-	path('edit/', views.ProfileUpdateView.as_view(), name='edit_profile')
+	path('edit/', views.ProfileUpdateView.as_view(), name='edit_profile'),
+	path(
+		'password/change/', 
+		auth_views.PasswordChangeView.as_view(
+			template_name='accounts/password_change/password_change.html',
+			success_url=reverse_lazy('accounts:password_change_done')
+		),
+		name='password_change'
+	),
+	path('password/change/done/', views.PasswordChangeDone.as_view(), name='password_change_done')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, TemplateView
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -48,3 +48,7 @@ class ProfileUpdateView(UpdateView, SearchFormMixin):
     def form_valid(self, form):
     	super().form_valid(form)
     	return redirect('blog:author_profile', pk=self.object.user.pk)
+
+@method_decorator(login_required, name='dispatch')
+class PasswordChangeDone(TemplateView, SearchFormMixin):
+    template_name = "accounts/password_change/password_change_done.html"
